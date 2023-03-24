8bdfd6c62109857a8d1f4894a040eac64f7a87a7
MythiliVJM <mythilivenugopal2002@gmail.com>
2023-03-24 13:06:43 +0530

    Mythili: Add Navigation

--- blog-website/src/components/navigation/Navbar.js
+++ blog-website/src/components/navigation/Navbar.js
@@ -1,7 +1,74 @@
-import React from 'react'
+import React  from 'react'
+import './Navstyle.css'
+import Profile from './profileImg.jpg'
+import Icon from './Writeicon.png'
+import Logo from './medium.png';
+
 
 export default function Navbar() {
+  
   return (
-    <div>Navbar</div>
+    <nav>
+        <div className='nav-1'>
+        
+        <img src={Logo} className="logo"/>
+        
+        <div className='Searchbar'>
+           <input type="text" placeholder="Search blog's here" />
+        </div>
+        </div>
+        <div className='nav-2'>
+        
+          <ul>
+            <li><img className='Icon' src={Icon} alt='write'/></li>
+            <li><a href='index.html'>Write</a></li>
+            <li><a href='index.html'>Your blog's</a></li>
+            <li><a href='index.html'>About</a></li>
+            <li><a href='index.html'>Log in</a></li>
+            <li><a href='index.html'>Sign out</a></li>
+          </ul>
+          <li><img className='Signin' src={Profile} alt='userImg' /></li>
+          
+          
+          {/* <div className='sub-menu-wrap' id='subMenu'>
+            <div className='sub-menu'>
+                <div className='user-info'>
+                    <img src={Profile} alt='user-profile'/> 
+                    <h2>MythiliVenugopal</h2>
+                </div>
+                <hr/>
+                <a href='#' className='sub-menu-link'>
+                  <p>Profile</p>
+                  <span>></span>
+                </a>
+                <a href='#' className='sub-menu-link'>
+                  <p>Library</p>
+                  <span>></span>
+                </a>
+                <a href='#' className='sub-menu-link'>
+                  <p>Stories</p>
+                  <span>></span>
+                </a>
+                <a href='#' className='sub-menu-link'>
+                  <p>Status</p>
+                  <span>></span>
+                </a>
+                <a href='#' className='sub-menu-link'>
+                  <p>Logout</p>
+                  <span>></span>
+                </a>
+            </div>
+          </div> */}
+        </div>
+    </nav>
   )
 }
+
+// function DropdownItems(props){
+//   return(
+//     <li className='dropdownItem'>
+//       {/* <img src={props.img}></img> */}
+//       <a>{props.text}</a>
+//     </li>
+//   )
+// }
